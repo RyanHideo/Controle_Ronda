b25559ae59b398c0fed94b46206610eb66b169b4
Ryan <ryanhougaske@gmail.com>
2026-04-24 15:26:36 -0400

    alter user service

--- src/main/java/com/sinapse/gerenciador/user/controller/UsersController.java
+++ src/main/java/com/sinapse/gerenciador/user/controller/UsersController.java
@@ -1,7 +1,9 @@
 package com.sinapse.gerenciador.user.controller;
 
+import com.sinapse.gerenciador.user.dto.UserResponseDTO;
 import com.sinapse.gerenciador.user.model.User;
 import com.sinapse.gerenciador.user.repository.UserRepository;
+import com.sinapse.gerenciador.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,42 +15,22 @@ import java.util.Optional;
 @RequestMapping("/users")
 public class UsersController {
 
-    private final UserRepository userRepository;
+    private  final UserService userService;
 
-    public UsersController(UserRepository userRepository){
-        this.userRepository = userRepository;
-    }
-
-    @PostMapping // Mapeia requisições POST para /users
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User savedUser = userRepository.save(user);
-        // Logica para salvar o usuário (geralmente chama um service)
-        System.out.println("Usuário recebido: " + user.getName());
-        // Retorna o usuário criado com status 201 Created
-        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = userRepository.findAll();
-        return new ResponseEntity<>(users, HttpStatus.OK);
+    public UsersController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id){
-        Optional<User> userOptional = userRepository.findById(id);
-        if (userOptional.isPresent()){
-            return new ResponseEntity<>(userOptional.get(),HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.buscarPorId(id));
     }
 
-    @GetMapping("/login/{login}")
-    public ResponseEntity<User> getUserByLogin(@PathVariable String login){
-        Optional<User> userOptional = userRepository.findByLogin(login);
-        if (userOptional.isPresent()){
-            return new ResponseEntity<>(userOptional.get(),HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/login")
+    public ResponseEntity<UserResponseDTO> getUserByLogin(@PathVariable String login){
+        return ResponseEntity.ok(userService.buscarPorLogin(login));
     }
+
+
+
 }
