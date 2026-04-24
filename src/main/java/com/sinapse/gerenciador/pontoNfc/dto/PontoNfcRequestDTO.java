74adc0e7bc53758d67fd742d22c47bc9b548493f
Ryan <ryanhougaske@gmail.com>
2026-04-24 09:10:59 -0400

    add: DTO in PontoNfc

---
+++ src/main/java/com/sinapse/gerenciador/pontoNfc/dto/PontoNfcRequestDTO.java
@@ -0,0 +1,62 @@
+package com.sinapse.gerenciador.pontoNfc.dto;
+
+import com.sinapse.gerenciador.pontoNfc.model.StatusPontoNfc;
+
+public class PontoNfcRequestDTO {
+
+    private String codigoNfc;
+    private String nome;
+    private Double latitude;
+    private Double longitude;
+    private Double raioPermitido;
+    private StatusPontoNfc status;
+
+
+    public String getCodigoNfc() {
+        return codigoNfc;
+    }
+
+    public void setCodigoNfc(String codigoNfc) {
+        this.codigoNfc = codigoNfc;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getRaioPermitido() {
+        return raioPermitido;
+    }
+
+    public void setRaioPermitido(Double raioPermitido) {
+        this.raioPermitido = raioPermitido;
+    }
+
+    public StatusPontoNfc getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPontoNfc status) {
+        this.status = status;
+    }
+}
--- src/main/java/com/sinapse/gerenciador/pontoNfc/service/PontoNfcService.java
+++ src/main/java/com/sinapse/gerenciador/pontoNfc/service/PontoNfcService.java
@@ -1,9 +1,49 @@
 package com.sinapse.gerenciador.pontoNfc.service;
 
 
+import com.sinapse.gerenciador.pontoNfc.model.PontoNfc;
+import com.sinapse.gerenciador.pontoNfc.dto.PontoNfcResponseDTO;
+import com.sinapse.gerenciador.pontoNfc.repository.PontoNfcRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PontoNfcService {
+
+    private final PontoNfcRepository pontoNfcRepository;
+
+    public PontoNfcService(PontoNfcRepository pontoNfcRepository)
+        {
+        this.pontoNfcRepository = pontoNfcRepository;
+    }
+
+    //Buscar ponto por id
+    public PontoNfcResponseDTO buscarPorId(Long id) {
+        PontoNfc ponto = pontoNfcRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Ponto NFC não encontrado"));
+
+        return toResponseDTO(ponto);
+    }
+    private PontoNfcResponseDTO toResponseDTO(PontoNfc ponto) {
+        if (ponto == null) return null;
+
+        PontoNfcResponseDTO dto = new PontoNfcResponseDTO();
+        dto.setId(ponto.getId());
+        dto.setCodigoNfc(ponto.getCodigoNfc());
+        dto.setNome(ponto.getNome());
+        dto.setLatitude(ponto.getLatitude());
+        dto.setLongitude(ponto.getLongitude());
+        dto.setRaioPermitido(ponto.getRaioPermitido());
+        dto.setStatus(ponto.getStatus());
+        return dto;
+    }
+
+    //Buscar por codigo nfc
+    public PontoNfcResponseDTO buscarPorCodigo(String codigoNfc) {
+        PontoNfc ponto = pontoNfcRepository.findByCodigoNfc(codigoNfc)
+                .orElseThrow(() -> new EntityNotFoundException("Ponto NFC não encontrado"));
+
+        return toResponseDTO(ponto);
+    }
 
 }
